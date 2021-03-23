04dee4892665fa8fd733d79b3ed7e9bbe6c45408
acoskunUSC <81244783+acoskunUSC@users.noreply.github.com>
2021-03-23 10:48:13 -0400

    Delete rmtrack/examples directory

--- rmtrack/examples/run-example.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-instance=$1
-method=$2
-showvis=$3
-avoidDeadlock=$4
-
-java -enableassertions -XX:+UseSerialGC -Xss4m -jar ../target/robust-tracking-1.0-SNAPSHOT-jar-with-dependencies.jar -method $method -problemfile $instance.xml -timestep 800 -maxtime 600000 -dprob 10 $showvis $avoidDeadlock
